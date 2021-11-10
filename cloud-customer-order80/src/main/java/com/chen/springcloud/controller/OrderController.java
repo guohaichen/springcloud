cd190c8b5230ab417748846643e7ba7369fa1f86
guohaichen <38747644+guohaichen@users.noreply.github.com>
2021-11-10 15:38:25 +0800

    commons

--- cloud-customer-order80/src/main/java/com/chen/springcloud/controller/OrderController.java
+++ cloud-customer-order80/src/main/java/com/chen/springcloud/controller/OrderController.java
@@ -5,7 +5,6 @@ import com.chen.springcloud.entity.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
